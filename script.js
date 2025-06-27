e6c7cbd034ced18d04f0bf6269180ab8997788e5
trunghieu8328 <139681975+trunghieu8328@users.noreply.github.com>
2025-06-27 20:41:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.querySelector('.login-btn').addEventListener('click', () => {
   alert('Tính năng đăng nhập đang phát triển!');
 });
+function toggleMenu() {
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("overlay");
+
+  sidebar.classList.toggle("open");
+  overlay.classList.toggle("show");
+}
