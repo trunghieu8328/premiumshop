99e12f7a5187d3dec4c11b4632d1339d12836a31
trunghieu8328 <139681975+trunghieu8328@users.noreply.github.com>
2025-06-27 20:44:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-document.querySelector('.login-btn').addEventListener('click', () => {
-  alert('Tính năng đăng nhập đang phát triển!');
-});
 function toggleMenu() {
   const sidebar = document.getElementById("sidebar");
   const overlay = document.getElementById("overlay");
